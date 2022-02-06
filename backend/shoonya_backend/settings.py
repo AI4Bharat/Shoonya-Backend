a631b086a59d7d2c633f9321f61a2b8c9316fbe7
Atharva Naphade <naphade21@gmail.com>
2022-02-06 15:40:43 +0530

    Added password field in database in settings.py

--- backend/shoonya_backend/settings.py
+++ backend/shoonya_backend/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT')
     }
