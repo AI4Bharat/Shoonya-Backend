5102bd66899b2bd4ae458b182273a22286f76b31
Atharva Naphade <naphade21@gmail.com>
2022-02-07 13:08:50 +0530

    Added Organizations and Invites

--- backend/organizations/models.py
+++ backend/organizations/models.py
@@ -1,3 +1,89 @@
-from django.db import models
+from django.db import models, transaction
+from shoonya_backend.settings import AUTH_USER_MODEL
+from users.models import User
+
+
+from shoonya_backend.mixins import DummyModelMixin
+
+from django.conf import settings
 
 # Create your models here.
+
+class Organization(models.Model, DummyModelMixin):
+    """
+    Organization Model
+    """
+
+    title = models.CharField(verbose_name='organization_title', max_length=1024, null=False)
+
+    email_domain_name = models.CharField(verbose_name='organization_email_domain', max_length=4096, null=True)
+
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='organizations')
+
+    is_active = models.BooleanField(verbose_name='organization_is_active', default=True, help_text=('Designates weather an organization is active or not.'))
+
+    created_by = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='organization', verbose_name='created_by')
+
+    created_at = models.DateTimeField(verbose_name='created_at', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='updated_at', auto_now=True)
+
+    def __str__(self):
+        return self.title + ', id=' + str(self.pk)
+
+    @classmethod
+    def create_organization(cls, created_by=None, title='Organization', email_domain_name='organization@shoonya.org'):
+        with transaction.atomic():
+            org = Organization.objects.create(created_by=created_by, title=title, email_domain_name=email_domain_name)
+            return org
+    
+    def add_user(self, user):
+        if self.users.filter(pk=user.pk).exists():
+            print('User exists!')
+            return
+        
+        with transaction.atomic():
+            self.users.add(user)
+            return
+
+    def has_user(self, user):
+        return self.users.filter(pk=user.pk)
+    
+    @property
+    def get_owner(self):
+        return self.created_by
+    
+    def has_permission(self, user):
+        if self in user.organizations.all():
+            return True
+        return False
+
+class Invite(models.Model):
+    """
+    Invites to invite users to organizations.
+    """
+    
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='invite_users')
+
+    organization = models.OneToOneField(Organization, on_delete=models.CASCADE, null=True, related_name='invite_oganization', verbose_name='organization')
+
+    invite_code = models.CharField(verbose_name='invite_code', max_length=256, null=True)
+
+    def __str__(self):
+        return str(self.organization.title) + ', ' + str(self.organization.created_by.email)
+    
+    @classmethod
+    def create_invite(cls, organization=None, users=None):
+        with transaction.atomic():
+            invite = Invite.objects.create(organization=organization)
+            for user in users:    
+                invite.users.add(user)
+            invite.save()
+            return invite
+    
+    def has_permission(self, user):
+        if self.organization.pk == user.pk or user.is_superuser:
+            return True
+        return False
+
+    def generate_invite_code(self):
+        pass
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import Group
 from .models import User
+from organizations.models import *
 from django.contrib import admin
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Organization)
+admin.site.register(Invite)
 
 admin.site.unregister(Group)
