d13e0ed6668cf137efd4ad8efc63d978d3e2b2b4
Atharva Naphade <naphade21@gmail.com>
2022-02-07 16:12:57 +0530

    added workspaces

--- backend/shoonya_backend/settings.py
+++ backend/shoonya_backend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'djoser',
     'users',
     'organizations',
+    'workspaces',
 ]
 
 MIDDLEWARE = [
---
+++ backend/workspaces/models.py
@@ -0,0 +1,30 @@
+from tabnanny import verbose
+from django.db import models
+from organizations.models import Organization
+from shoonya_backend.mixins import DummyModelMixin
+
+from shoonya_backend import settings
+
+# Create your models here.
+
+class Workspace(models.Model, DummyModelMixin):
+    """
+    Workspace model.
+    """
+    organization = models.ForeignKey(Organization, related_name='organization_workspace', on_delete=models.CASCADE)
+
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='organization_users')
+
+    workspace_name = models.CharField(verbose_name='workspace_name', max_length=256, null=False)
+
+    is_archived = models.BooleanField(verbose_name='is_archived', default=False)
+
+    created_by = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name='created_by', related_name='workspace_manager', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.workspace_name) + ', ' + str(self.created_by.email)
+
+    def has_user(self, user):
+        if self.user.filter(pk=user.pk).exists():
+            return True
+        return False
