051838e64b29029c9ed61e8c2be5794e507742d5
Rugz007 <dsweb007@gmail.com>
2022-02-14 10:36:57 +0530

    Commented working of test cases

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -5,13 +5,12 @@ from django.urls import reverse
 from .models import User
 from .views import *
 
-# Create your tests here.
-
 
 class UserTestcase(APITestCase):
     client = APIClient()
 
     def setUp(self):
+        # Setting up a dummy organization and creating a superuser for testing purposes.
         Organization.objects.create(title="Test Organization")
         User.objects.create_superuser(username="admin", email="admin@admin.com", password="admin")
 
@@ -19,11 +18,18 @@ class UserTestcase(APITestCase):
         """
         Check invite user API.
         """
+        # Creating Request data
         data = {"emails": ["sample@email.com", "sampl1e@email.com",], "organization_id": 1}
+        # Giving the correct expected response from the view
         correct_response = {"message": "Invite sent"}
+        # Writing the URL of the API to be tested [Format is appname:basename-urlname]
+        # For basename, refer urls.py of that app. For urlname, refer the action decorator of the viewset function.
         url = reverse("users:invite-invite_users")
+        # Logging in as the admin user
         self.client.login(email="admin@admin.com", password="admin")
+        # Sending the request to the API
         response = self.client.post(url, data, format="json")
+        # Checking if the response is correct
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, correct_response)
 
