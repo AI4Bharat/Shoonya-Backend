552da062cdcdffb0241a070ed1c9c119eeda78fd
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2022-02-16 16:50:55 +0530

    Fixed Dockerfile and README

--- backend/shoonya_backend/settings.py
+++ backend/shoonya_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('ENV') == 'dev')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definition
