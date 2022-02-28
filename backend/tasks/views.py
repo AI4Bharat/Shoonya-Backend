4fe87f931f2db7622a0f81ecf250139d1fbfdf1c
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2022-02-28 17:09:04 +0530

    Added permission classes and doc strings

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -3,6 +3,7 @@ from rest_framework import mixins
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from tasks.models import Task, Annotation
 from tasks.serializers import TaskSerializer, AnnotationSerializer
@@ -12,11 +13,19 @@ from users.models import User
 # Create your views here.
 
 class TaskViewSet(viewsets.ModelViewSet):
+    """
+        Model Viewset for Tasks. All Basic CRUD operations are covered here.
+    """
+
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     @action(detail=True, methods=['post'], url_path='assign')
     def assign(self, request, pk):
+        """
+            Assigns users with the given user IDs to the particular task.
+        """
         task = self.get_object()
         user_ids = request.data.get('user_ids')
         users = []
@@ -30,6 +39,9 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['get'], url_path='annotations')
     def annotations(self, request, pk):
+        """
+            Returns all the annotations associated with a particular task.
+        """
         task = self.get_object()
         annotations = Annotation.objects.filter(task=task)
         serializer = AnnotationSerializer(annotations, many=True)
@@ -38,5 +50,9 @@ class TaskViewSet(viewsets.ModelViewSet):
         
 
 class AnnotationViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+    """
+        Annotation Viewset with create and update operations.
+    """
     queryset = Annotation.objects.all()
     serializer_class = AnnotationSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
