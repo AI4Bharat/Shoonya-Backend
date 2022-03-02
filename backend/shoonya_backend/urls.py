428b86c97f6cf26e859e65aed9f23c09b2be1284
kaushal-py <kaushalb@iisc.ac.in>
2022-03-02 16:56:38 +0530

    Fix pylint errors

--- backend/shoonya_backend/urls.py
+++ backend/shoonya_backend/urls.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
+SchemaView = get_schema_view(
     openapi.Info(
         title="Snippets API",
         default_version="v1",
@@ -26,15 +26,15 @@ urlpatterns = [
     path("data/", include("dataset.urls")),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
+        SchemaView.without_ui(cache_timeout=0),
         name="schema-json",
     ),
     re_path(
         r"^swagger/$",
-        schema_view.with_ui("swagger", cache_timeout=0),
+        SchemaView.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
     re_path(
-        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+        r"^redoc/$", SchemaView.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
 ]
