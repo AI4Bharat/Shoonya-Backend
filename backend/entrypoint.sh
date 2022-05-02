6762ce26b38c330b1281439da7842faaa5816ed2
Atharva Naphade <naphade21@gmail.com>
2022-05-02 17:29:06 +0530

    Reverted docker config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,6 @@ python manage.py makemigrations users
 python manage.py makemigrations organizations
 python manage.py makemigrations workspaces
 python manage.py migrate
-python manage.py collectstatic --no-input -v 2 --clear
+python manage.py collectstatic --no-input --clear
 
 exec "$@"
