1693c663343ddea1666097c5c458e48ba1715fae
Atharva Naphade <naphade21@gmail.com>
2022-05-31 08:58:43 +0530

    Update setings.py

--- backend/shoonya_backend/settings.py
+++ backend/shoonya_backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
 ENV_ALLOWED_HOST = os.environ.get("ENV_ALLOWED_HOST")
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 if ENV_ALLOWED_HOST:
     ALLOWED_HOSTS = [ ENV_ALLOWED_HOST ]
 # Application definition
