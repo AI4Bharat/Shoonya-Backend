f892a4f2c5aaedb780281e4346d015fafd7704c5
Prakhar Rathi <prakharrathi25@gmail.com>
2022-06-07 13:56:22 +0530

    Added changes to handle the pylint issues

--- backend/shoonya_backend/celery.py
+++ backend/shoonya_backend/celery.py
@@ -6,7 +6,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoonya_backend.settings')
 
 # Define celery app and settings
-celery_app = Celery('shoonya_backend', 
+celery_app = Celery('shoonya_backend',
                     result_backend = 'django-db',
                     accept_content = ['application/json'],
                     result_serializer = 'json',
@@ -17,4 +17,5 @@ celery_app.autodiscover_tasks()
 
 @celery_app.task(bind=True)
 def debug_task(self):
+    ''' First task for task handling testing and to apply migrations to the celery results db '''
     print(f'Request: {self.request!r}')
