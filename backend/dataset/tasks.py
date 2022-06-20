90993be1bc99e26f23681d2b127e19ee24de2fae
Prakhar Rathi <prakharrathi25@gmail.com>
2022-06-20 17:05:07 +0530

    Update tasks.py
    Update task formatting

--- backend/dataset/tasks.py
+++ backend/dataset/tasks.py
@@ -1,7 +1,5 @@
 import traceback
 from celery import shared_task
-from rest_framework import status
-from rest_framework.response import Response
 from tablib import Dataset
 
 from .resources import RESOURCE_MAP
@@ -10,12 +8,16 @@ from .resources import RESOURCE_MAP
 
 
 @shared_task(
-    bind=True, 
-    autoretry_for=(Exception,), 
-    exponential_backoff=2, 
-    retry_kwargs={'max_retries': 1})
-def upload_data_to_data_instance(self, dataset_string, pk, dataset_type): 
-    """Celery background task to upload the data to the dataset instance through CSV  
+    bind=True,
+    autoretry_for=(Exception,),
+    exponential_backoff=2,
+    retry_kwargs={
+        "max_retries": 5,
+        "countdown": 2,
+    },
+)
+def upload_data_to_data_instance(self, dataset_string, pk, dataset_type):
+    """Celery background task to upload the data to the dataset instance through CSV
 
     Args:
         dataset_string (str): The CSV data to be uploaded in string format
@@ -24,10 +26,10 @@ def upload_data_to_data_instance(self, dataset_string, pk, dataset_type):
     """
 
     # Create a new tablib Dataset and load the data into this dataset
-    imported_data = Dataset().load(dataset_string, format='csv')
+    imported_data = Dataset().load(dataset_string, format="csv")
 
     # Add the instance_id column to all rows in the dataset
-    imported_data.append_col([pk]*len(imported_data), header="instance_id")
+    imported_data.append_col([pk] * len(imported_data), header="instance_id")
 
     # Declare the appropriate resource map based on dataset type
     resource = RESOURCE_MAP[dataset_type]()
@@ -35,14 +37,14 @@ def upload_data_to_data_instance(self, dataset_string, pk, dataset_type):
     # Import the data into the database
     try:
         resource.import_data(imported_data, raise_errors=True)
-    
+
     # If validation checks fail, raise the Exception
     except Exception as e:
 
         self.update_state(
-            state='FAILURE', 
+            state="FAILURE",
             meta={
-                'exc_type': type(e).__name__,
-                'exc_message': traceback.format_exc().split('\n'),
-            }            
+                "exc_type": type(e).__name__,
+                "exc_message": traceback.format_exc().split("\n"),
+            },
         )
