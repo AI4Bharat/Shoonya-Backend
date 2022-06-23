4ce20c906bd864c6443766364e21e4a09d292e8c
Prakhar Rathi <prakharrathi25@gmail.com>
2022-06-23 10:07:34 +0530

    Move the word count function to project.utils

--- backend/projects/utils.py
+++ backend/projects/utils.py
@@ -1,4 +1,6 @@
+import re
 from typing import Tuple
+
 from dateutil.parser import parse as date_parse
 
 
@@ -15,3 +17,17 @@ def is_valid_date(s: str) -> Tuple[bool, str]:
         return (False, "Please select dates upto Today")
 
     return (True, "")
+
+def no_of_words(string):
+    """Return a the number of words in a given string
+
+    Args:
+        string (str): A string to count the number of words in 
+
+    Returns:
+        int: The number of words in a string. 
+    """
+
+    filter1 = [word for word in string.split() if len(word) > 1]
+    filter2 = [word for word in filter1 if re.search('[a-zA-Z]', word) != None]
+    return len(filter2) 
