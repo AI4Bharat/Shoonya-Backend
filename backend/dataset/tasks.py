d5bbc98ca5617157efd089be68e5b0701e475ebe
Prakhar Rathi <prakharrathi25@gmail.com>
2022-06-28 13:58:49 +0530

    Make CSV data upload iterative
    - Made the function inside celery task file, iterate over rows in an iterative fashion

--- backend/dataset/tasks.py
+++ backend/dataset/tasks.py
@@ -13,11 +13,12 @@ from .resources import RESOURCE_MAP
     autoretry_for=(Exception,),
     exponential_backoff=2,
     retry_kwargs={
-        "max_retries": 5,
+        "max_retries": 1,
         "countdown": 2,
     },
 )
 def upload_data_to_data_instance(self, dataset_string, pk, dataset_type, content_type):
+    # sourcery skip: raise-specific-error
     """Celery background task to upload the data to the dataset instance through file upload
 
     Args:
@@ -36,20 +37,55 @@ def upload_data_to_data_instance(self, dataset_string, pk, dataset_type, content
     # Add the instance_id column to all rows in the dataset
     imported_data.append_col([pk] * len(imported_data), header="instance_id")
 
+    # Add row numbers to the dataset
+    imported_data.append_col(range(1, len(imported_data) + 1), header="row_number")
+
     # Declare the appropriate resource map based on dataset type
     resource = RESOURCE_MAP[dataset_type]()
 
-    # Import the data into the database and return Success if all checks are passed
-    try:
-        resource.import_data(imported_data, raise_errors=True)
+    # List with row numbers that couldn't be uploaded 
+    failed_rows = []
 
-    # If validation checks fail, raise the Exception
-    except Exception as e:
+    # Iterate through the dataset and upload each row to the database
+    for row in imported_data.dict:
+
+        # Remove row number column from the row being uploaded
+        row_number = row['row_number']
+        del row['row_number']
+
+        # Convert row to a tablib dataset
+        row_dataset = Dataset(row)
+
+        upload_result = resource.import_data(row_dataset, raise_errors=False)
+
+        # check if the upload result has errors 
+        if (upload_result.has_errors() or upload_result.has_validation_errors()):
+            failed_rows.append(row_number)
+
+    # # Import the data into the database and return Success if all checks are passed
+    # try:
+    #     resource.import_data(imported_data, raise_errors=True)
+
+    # If there are upload errors return the failed rows and make the task a failure
+    if failed_rows:
         self.update_state(
             state="FAILURE",
             meta={
-                "exc_type": type(e).__name__,
-                "exc_message": traceback.format_exc().split("\n"),
+                "failed_line_numbers": failed_rows,
             },
         )
-        raise e
+        raise Exception(f"Upload failed for lines: {failed_rows}")
+    else: 
+        return "All rows uploaded."
+
+
+    # If validation checks fail, raise the Exception
+    # except Exception as e:
+    #     self.update_state(
+    #         state="FAILURE",
+    #         meta={
+    #             "exc_type": type(e).__name__,
+    #             "exc_message": traceback.format_exc().split("\n"),
+    #         },
+    #     )
+    #     raise e
