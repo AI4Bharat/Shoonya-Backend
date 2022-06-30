28221906c1e4b412604030fcb7e7a4e155b8cf52
dheeraj gujral <dheerajgujral47@gmail.com>
2022-06-30 16:09:33 +0530

    Resolving migration conflicts

--- backend/tasks/migrations/0029_auto_20220624_1450.py
+++ backend/tasks/migrations/0029_auto_20220624_1450.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('tasks', '0028_auto_20220617_0730'),
+        ('tasks', '0029_merge_20220624_1106'),
     ]
 
     operations = [
