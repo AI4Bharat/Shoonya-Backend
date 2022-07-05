e15a79707bf040d1b58e7d8456254ab274e899f8
Prakhar Rathi <prakharrathi25@gmail.com>
2022-07-05 16:49:13 +0530

    Add the translation feature

--- backend/functions/views.py
+++ backend/functions/views.py
@@ -1,33 +1,36 @@
 import json
 
 from dataset import models as dataset_models
-from googletrans import Translator
 from projects.models import *
+from dataset.serializers import SentenceTextSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from tasks.models import *
+from .utils import GoogleTranslator
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def copy_from_block_text_to_sentence_text(request):
     """
-    Copies each sentence from a block of text to sentence text daatset 
+    Copies each sentence from a block of text to sentence text daatset
     """
-    export_dataset_instance_id = request.data['export_dataset_instance_id']
-    project_id = request.data['project_id']
+    export_dataset_instance_id = request.data["export_dataset_instance_id"]
+    project_id = request.data["project_id"]
     project = Project.objects.get(pk=project_id)
 
     # if project.is_archived == False:
-    #     ret_dict = {"message": "Project is not archived!"}         
+    #     ret_dict = {"message": "Project is not archived!"}
     #     ret_status = status.HTTP_403_FORBIDDEN
     #     return Response(ret_dict, status=ret_status)
     # if "copy_from_block_text_to_sentence_text" in project.metadata_json:
-    #     ret_dict = {"message": "Function already applied on this project!"}         
+    #     ret_dict = {"message": "Function already applied on this project!"}
     #     ret_status = status.HTTP_403_FORBIDDEN
     #     return Response(ret_dict, status=ret_status)
 
-    export_dataset_instance = dataset_models.DatasetInstance.objects.get(instance_id__exact=export_dataset_instance_id)
+    export_dataset_instance = dataset_models.DatasetInstance.objects.get(
+        instance_id__exact=export_dataset_instance_id
+    )
     # dataset_model = dataset_models.BlockText
     # input_instances = project.dataset_id
     tasks = Task.objects.filter(project_id__exact=project)
@@ -48,49 +51,49 @@ def copy_from_block_text_to_sentence_text(request):
             sentences = raw_text.split("\n")
             for sentence in sentences:
                 sentence_text = dataset_models.SentenceText(
-                    parent_data = block_text,
+                    parent_data=block_text,
                     language=block_text.language,
                     text=sentence,
                     domain=block_text.domain,
-                    instance_id = export_dataset_instance,
+                    instance_id=export_dataset_instance,
                 )
                 all_sentence_texts.append(sentence_text)
-            task.metadata_json["copy_from_block_text_to_sentence_text"]=True
+            task.metadata_json["copy_from_block_text_to_sentence_text"] = True
             task.task_status = FREEZED
             task.save()
-
 
     # TODO: implement bulk create if possible (only if non-hacky)
     for sentence_text in all_sentence_texts:
         sentence_text.save()
     # dataset_models.SentenceText.objects.bulk_create(all_sentence_texts)
-    
-    
+
     project.save()
-    ret_dict = {"message": "SUCCESS!"}         
+    ret_dict = {"message": "SUCCESS!"}
     ret_status = status.HTTP_200_OK
     return Response(ret_dict, status=ret_status)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def copy_from_ocr_document_to_block_text(request):
     """
-    Copies data annotated from OCR Document to Block text Dataset 
+    Copies data annotated from OCR Document to Block text Dataset
     """
-    export_dataset_instance_id = request.data['export_dataset_instance_id']
-    project_id = request.data['project_id']
+    export_dataset_instance_id = request.data["export_dataset_instance_id"]
+    project_id = request.data["project_id"]
     project = Project.objects.get(pk=project_id)
 
     # if project.is_archived == False:
-    #     ret_dict = {"message": "Project is not archived!"}         
+    #     ret_dict = {"message": "Project is not archived!"}
     #     ret_status = status.HTTP_403_FORBIDDEN
     #     return Response(ret_dict, status=ret_status)
     # if "copy_from_ocr_document_to_block_text" in project.metadata_json:
-    #     ret_dict = {"message": "Function already applied on this project!"}         
+    #     ret_dict = {"message": "Function already applied on this project!"}
     #     ret_status = status.HTTP_403_FORBIDDEN
     #     return Response(ret_dict, status=ret_status)
 
-    export_dataset_instance = dataset_models.DatasetInstance.objects.get(instance_id__exact=export_dataset_instance_id)
+    export_dataset_instance = dataset_models.DatasetInstance.objects.get(
+        instance_id__exact=export_dataset_instance_id
+    )
     # dataset_model = dataset_models.BlockText
     # input_instances = project.dataset_id
     tasks = Task.objects.filter(project_id__exact=project)
@@ -102,7 +105,9 @@ def copy_from_ocr_document_to_block_text(request):
         if task.output_data is not None:
             if "copy_from_ocr_document_to_block_text" in task.metadata_json:
                 continue
-            ocr_document = dataset_models.OCRDocument.objects.get(id=task.output_data.id)
+            ocr_document = dataset_models.OCRDocument.objects.get(
+                id=task.output_data.id
+            )
             # block_text = task.output_data
             # block_text.__class__ = dataset_models.BlockText
             # block_text = dataset_models.BlockText()
@@ -114,46 +119,72 @@ def copy_from_ocr_document_to_block_text(request):
             labels = json.loads(labels)
 
             body_transcriptions = []
-            for i,label in enumerate(labels):
-                if label['labels'][0] == 'Body':
+            for i, label in enumerate(labels):
+                if label["labels"][0] == "Body":
                     body_transcriptions.append(transcriptions[i])
 
             text = " ".join(body_transcriptions)
             # TODO: check if domain can be same as OCR domain
             block_text = dataset_models.BlockText(
-                parent_data = ocr_document,
+                parent_data=ocr_document,
                 language=ocr_document.language,
                 text=text,
                 domain=ocr_document.ocr_domain,
-                instance_id = export_dataset_instance,
+                instance_id=export_dataset_instance,
             )
             all_block_texts.append(block_text)
-            task.metadata_json["copy_from_ocr_document_to_block_text"]=True
+            task.metadata_json["copy_from_ocr_document_to_block_text"] = True
             task.task_status = FREEZED
             task.save()
-
 
     # TODO: implement bulk create if possible (only if non-hacky)
     for block_text in all_block_texts:
         block_text.save()
     # dataset_models.SentenceText.objects.bulk_create(all_sentence_texts)
-    
+
     # project.metadata_json["copy_from_ocr_document_to_block_text"]=True
     # project.save()
-    ret_dict = {"message": "SUCCESS!"}         
+    ret_dict = {"message": "SUCCESS!"}
     ret_status = status.HTTP_200_OK
     return Response(ret_dict, status=ret_status)
+
 
 @api_view(["POST"])
 def schedule_google_translate_job(request):
     """
-    Schedules a Google Translate job for a given project
+    Schedules a Google Translate job for a given dataset instance
+
+    Request Body
+    {
+        "input_dataset_instance_id": <int>,
+        "languages": <list>
+    }
+
+    Response Body
+    {
+        "message": <str>
+        "result": <str>
+        "status": DjangoStatusCode
+    }
     """
 
-    input_dataset_instance_id = request.data['input_dataset_instance_id']
-    languages = request.data['languages']
+    input_dataset_instance_id = request.data["input_dataset_instance_id"]
+    languages = request.data["languages"]
 
-    ret_dict = {"message": "SUCCESS!"}         
+    # Collect the sentences from Sentence Text based on dataset id
+    input_sentences = list(
+        dataset_models.SentenceText.objects.filter(
+            instance_id=input_dataset_instance_id
+        ).values_list("context")
+    )
+
+    # Create a google translator object
+    translator = GoogleTranslator()
+
+    # Get the google translations for the given dataset instance and languages
+    # for lang in languages:
+    #     result = translator.batch_translate(sentences=input_sentences, input_lang=, output_lang: str, batch_size: int = 100)
+
+    ret_dict = {"message": "SUCCESS!", "result": input_sentences}
     ret_status = status.HTTP_200_OK
     return Response(ret_dict)
-
