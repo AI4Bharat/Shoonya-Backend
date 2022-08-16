00feb1482cd016e266f50a2ead5c85b6cefd5ba3
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2022-08-16 13:07:18 +0530

    migration file reformated6

--- backend/tasks/migrations/0031_auto_20220812_0936.py
+++ backend/tasks/migrations/0031_auto_20220812_0936.py
@@ -9,18 +9,31 @@ reverse_query = "UPDATE tasks_task SET task_status = 'rejected'  WHERE task_stat
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tasks', '0030_auto_20220707_0959'),
+        ("tasks", "0030_auto_20220707_0959"),
     ]
 
     operations = [
         migrations.RunSQL(
             sql=sql_query,
             reverse_sql=reverse_query,
-            ),
+        ),
         migrations.AlterField(
-            model_name='task',
-            name='task_status',
+            model_name="task",
+            name="task_status",
             field=models.CharField(
-                choices=[('unlabeled', 'unlabeled'), ('labeled', 'labeled'), ('skipped', 'skipped'), ('accepted', 'accepted'), ('accepted_with_changes', 'accepted_with_changes'), ('freezed', 'freezed'), ('to_be_revised', 'to_be_revised'), ('draft', 'draft')],
-                default='unlabeled', max_length=100, verbose_name='task_status'),
-        ), ]
+                choices=[
+                    ("unlabeled", "unlabeled"),
+                    ("labeled", "labeled"),
+                    ("skipped", "skipped"),
+                    ("accepted", "accepted"),
+                    ("accepted_with_changes", "accepted_with_changes"),
+                    ("freezed", "freezed"),
+                    ("to_be_revised", "to_be_revised"),
+                    ("draft", "draft"),
+                ],
+                default="unlabeled",
+                max_length=100,
+                verbose_name="task_status",
+            ),
+        ),
+    ]
