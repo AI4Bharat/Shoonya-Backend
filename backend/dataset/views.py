3ad0e83bfe8624feeee3bdd265bfc7a54c965d16
ummegith <ummaimaec.rymec@gmail.com>
2022-08-19 13:13:11 +0530

    updated after conf;licts

--- backend/dataset/views.py
+++ backend/dataset/views.py
@@ -12,6 +12,7 @@ from projects.serializers import ProjectSerializer
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import DatasetInstancePermission
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from organizations.decorators import (
