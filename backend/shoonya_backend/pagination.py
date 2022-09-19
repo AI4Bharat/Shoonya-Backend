4bf629fcc4f4a0f8fbb4beab2ed6ce1e817ff445
Ishvinder Sethi <ishvindersethi22@gmail.com>
2022-09-19 13:11:18 +0530

    Black linters

--- backend/shoonya_backend/pagination.py
+++ backend/shoonya_backend/pagination.py
@@ -8,8 +8,9 @@ DEFAULT_PAGE_SIZE = 10
 def replace_query_param(self, url, param, value):
     return url.replace(f"{param}={value}", f"{param}={value + 1}")
 
+
 def remove_query_param(url, key):
-    #Given a URL and a key/val pair, remove an item in the queryparameters of the URL, and return the new URL
+    # Given a URL and a key/val pair, remove an item in the queryparameters of the URL, and return the new URL
     def remove_query_param(url, key):
         scheme, netloc, path, query_string, fragment = urlsplit(url)
         query_params = parse_qs(query_string)
@@ -31,20 +32,18 @@ class CustomPagination(PageNumberPagination):
 
 # override the get_next_link method to return None if there is no next page and check if the page number is equal to 1
 def get_next_link(self):
-        if not self.page.has_next():
-            return None
-        url = self.request.build_absolute_uri()
-        page_number = self.page.next_page_number()
-        return replace_query_param(url, self.page_query_param, page_number)
+    if not self.page.has_next():
+        return None
+    url = self.request.build_absolute_uri()
+    page_number = self.page.next_page_number()
+    return replace_query_param(url, self.page_query_param, page_number)
 
 
 def get_previous_link(self):
-        if not self.page.has_previous():
-            return None
-        url = self.request.build_absolute_uri()
-        page_number = self.page.previous_page_number()
-        if page_number == 1:
-            return remove_query_param(url, self.page_query_param)
-        return replace_query_param(url, self.page_query_param, page_number)
-
-
+    if not self.page.has_previous():
+        return None
+    url = self.request.build_absolute_uri()
+    page_number = self.page.previous_page_number()
+    if page_number == 1:
+        return remove_query_param(url, self.page_query_param)
+    return replace_query_param(url, self.page_query_param, page_number)
