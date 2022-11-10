cc1699c44f804f9fc9a0def82cf63ccbfa70f909
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2022-11-10 12:13:39 +0530

    added public end point for cummulative task count in org level

--- backend/organizations/urls.py
+++ backend/organizations/urls.py
@@ -1,10 +1,12 @@
 from rest_framework import routers
 
 from .views import OrganizationViewSet
+from .views import OrganizationPublicViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r"", OrganizationViewSet, basename="organization")
+router.register(r"public", OrganizationPublicViewSet, basename="public")
 
 
 urlpatterns = router.urls
