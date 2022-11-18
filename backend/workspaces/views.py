1ca2b08de2d9b38149e393da49823e14c91b38ba
Ishvinder Sethi <ishvindersethi22@gmail.com>
2022-11-17 22:46:32 -0800

    Update views.py

--- backend/workspaces/views.py
+++ backend/workspaces/views.py
@@ -254,10 +254,7 @@ class WorkspaceViewSet(viewsets.ModelViewSet):
     
     @is_particular_workspace_manager
     def list(self, request, *args, **kwargs):
-        if (
-            int(request.user.role) == User.ANNOTATOR
-            or int(request.user.role) == User.WORKSPACE_MANAGER
-        ):
+        if int(request.user.role) == User.WORKSPACE_MANAGER:
             data = self.queryset.filter(
                 members=request.user,
                 is_archived=False,
