418248f4d4db0ed9ed3995ec528d02d916112028
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2022-12-14 16:37:58 +0530

    migration file conflict fixed

--- backend/tasks/migrations/0036_auto_20221116_0541.py
+++ backend/tasks/migrations/0036_auto_20221116_0541.py
@@ -6,18 +6,44 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tasks', '0035_auto_20221107_0539'),
+        ("tasks", "0035_auto_20221107_0539"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='annotation',
-            name='annotation_status',
-            field=models.CharField(choices=[('unlabeled', 'unlabeled'), ('labeled', 'labeled'), ('skipped', 'skipped'), ('draft', 'draft'), ('unreviewed', 'unreviewed'), ('accepted', 'accepted'), ('to_be_revised', 'to_be_revised'), ('accepted_with_minor_changes', 'accepted_with_minor_changes'), ('accepted_with_major_changes', 'accepted_with_major_changes')], default='unlabeled', max_length=100, verbose_name='annotation_status'),
+            model_name="annotation",
+            name="annotation_status",
+            field=models.CharField(
+                choices=[
+                    ("unlabeled", "unlabeled"),
+                    ("labeled", "labeled"),
+                    ("skipped", "skipped"),
+                    ("draft", "draft"),
+                    ("unreviewed", "unreviewed"),
+                    ("accepted", "accepted"),
+                    ("to_be_revised", "to_be_revised"),
+                    ("accepted_with_minor_changes", "accepted_with_minor_changes"),
+                    ("accepted_with_major_changes", "accepted_with_major_changes"),
+                ],
+                default="unlabeled",
+                max_length=100,
+                verbose_name="annotation_status",
+            ),
         ),
         migrations.AlterField(
-            model_name='task',
-            name='task_status',
-            field=models.CharField(choices=[('incomplete', 'incomplete'), ('annotated', 'annotated'), ('reviewed', 'reviewed'), ('Exported', 'Exported'), ('freezed', 'freezed')], default='unlabeled', max_length=100, verbose_name='task_status'),
+            model_name="task",
+            name="task_status",
+            field=models.CharField(
+                choices=[
+                    ("incomplete", "incomplete"),
+                    ("annotated", "annotated"),
+                    ("reviewed", "reviewed"),
+                    ("Exported", "Exported"),
+                    ("freezed", "freezed"),
+                ],
+                default="incomplete",
+                max_length=100,
+                verbose_name="task_status",
+            ),
         ),
     ]
