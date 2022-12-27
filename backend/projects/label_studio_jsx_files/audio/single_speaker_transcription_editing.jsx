79c94e47a55d8661a42b25a64e54847e12fe99b6
Ishvinder Sethi <ishvindersethi22@gmail.com>
2022-12-26 22:27:34 -0800

    Update single_speaker_transcription_editing.jsx

--- backend/projects/label_studio_jsx_files/audio/single_speaker_transcription_editing.jsx
+++ backend/projects/label_studio_jsx_files/audio/single_speaker_transcription_editing.jsx
@@ -1,7 +1,8 @@
 <View> 
   <Labels name="labels" toName="audio_url" className="ignore_assertion"> 
-    <Label value="Speaker" /> 
-    <Label value="Noise" /> 
+    {speaker_0_details ? <Label value="$speaker_0_details" />  : null} 
+    {speaker_1_details ? <Label value="$speaker_1_details" />  : null}
+    <Label value="Noise" />
   </Labels> 
   <AudioPlus name="audio_url" value="$audio_url"/> 
   
@@ -12,9 +13,10 @@
   <TextArea name="transcribed_json" toName="audio_url" 
             rows="2" editable="true" 
             perRegion="true" required="true" /> 
-   
-  {reference_raw_transcript ? <Text name="reference_raw_transcript" 
-value="$reference_raw_transcript"/> : null} 
+              
+  {reference_raw_transcript ? <Header value="Reference Transcript" /> 
+   <Text name="reference_raw_transcript" 
+    value="$reference_raw_transcript"/> : null} 
    
 </View> 
  
