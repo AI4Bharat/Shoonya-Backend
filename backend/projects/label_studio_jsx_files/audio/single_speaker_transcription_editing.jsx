985150c46c2102a4297ff92e6f13598c687e6a15
Ishvinder Sethi <ishvindersethi22@gmail.com>
2022-12-26 23:09:47 -0800

    Update single_speaker_transcription_editing.jsx

--- backend/projects/label_studio_jsx_files/audio/single_speaker_transcription_editing.jsx
+++ backend/projects/label_studio_jsx_files/audio/single_speaker_transcription_editing.jsx
@@ -1,7 +1,16 @@
 <View> 
+   <Header value="Speaker Details" />          
+   {$speaker_0_details ?
+   <Text name="Speaker 0 Details" 
+    value="$speaker_0_details"/> : null} 
+   {$speaker_1_details ?
+   <Text name="Speaker 1 Details" 
+    value="$speaker_1_details"/> : null} 
+
+
   <Labels name="labels" toName="audio_url" className="ignore_assertion"> 
-    {speaker_0_details ? <Label value="$speaker_0_details" />  : null} 
-    {speaker_1_details ? <Label value="$speaker_1_details" />  : null}
+    {speaker_0_details ? <Label value="$speaker_0_details.split(",")[0].split(": ")[1]" />  : null} 
+    {speaker_1_details ? <Label value="$speaker_1_details.split(",")[0].split(": ")[1]" />  : null}
     <Label value="Noise" />
   </Labels> 
   <AudioPlus name="audio_url" value="$audio_url"/> 
