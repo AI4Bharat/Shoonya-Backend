65dba072d3055004799068a67f767d48711bf770
Ishvinder Sethi <ishvindersethi22@gmail.com>
2023-01-11 23:02:29 -0800

    Update single_speaker_transcription_editing.jsx

--- backend/projects/label_studio_jsx_files/audio/single_speaker_transcription_editing.jsx
+++ backend/projects/label_studio_jsx_files/audio/single_speaker_transcription_editing.jsx
@@ -17,10 +17,11 @@
   <View visibleWhen="region-selected"> 
     <Header value="Provide Transcription" /> 
   </View> 
-  
+  <View style="width: 50%; padding-right: 2em; margin-left: 2em;">
   <TextArea name="transcribed_json" toName="audio_url" 
             rows="2" editable="true" 
             perRegion="true" required="true" /> 
+  </View>
               
   {reference_raw_transcript ? <Header value="Reference Transcript" /> 
    <Text name="reference_raw_transcript" 
