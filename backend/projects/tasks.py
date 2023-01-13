69eb41ab9c7e2054ccad59d685bcbeaee59c0392
krishna-vasudev <debrajisothermal@gmail.com>
2023-01-13 19:36:59 +0530

    changes for new project type contextual sentence verification and domain classification

--- backend/projects/tasks.py
+++ backend/projects/tasks.py
@@ -300,10 +300,20 @@ def export_project_in_place(
             # annotation_dict['result'] = annotation_dict['result_json']
             # del annotation_dict['result_json']
             task_dict["annotations"] = [OrderedDict(annotation_dict)]
+            print(task_dict)
+            if (
+                str(project.project_type)
+                == "ContextualSentenceVerificationAndDomainClassification"
+            ):
+                for idx, result in enumerate(task_dict["annotations"][0]["result"]):
+                    if result["from_name"] == "domain":
+                        task_dict["annotations"][0]["result"][idx]["value"][
+                            "taxonomy"
+                        ] = result["value"]["taxonomy"][0]
         del task_dict["annotation_users"]
         del task_dict["review_user"]
         tasks_list.append(OrderedDict(task_dict))
-
+    print(tasks_list[0])
     if output_dataset_info["dataset_type"] == "Conversation":
         tasks_annotations = tasks_list
     else:
@@ -314,6 +324,7 @@ def export_project_in_place(
         tasks_annotations = json.loads(tasks_df.to_json(orient="records"))
 
     for (ta, tl, task) in zip(tasks_annotations, tasks_list, annotated_tasks):
+        print(ta)
         if output_dataset_info["dataset_type"] == "SpeechConversation":
             ta_labels = json.loads(ta["labels"])
             ta_transcribed_json = json.loads(ta["transcribed_json"])
