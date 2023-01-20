8d18afd320eccb184907ef26a41d17c258a1d987
Ishvinder Sethi <ishvindersethi22@gmail.com>
2023-01-20 03:56:18 -0800

    Update single_speaker_transcription_editing.jsx

--- backend/projects/label_studio_jsx_files/audio/single_speaker_transcription_editing.jsx
+++ backend/projects/label_studio_jsx_files/audio/single_speaker_transcription_editing.jsx
@@ -19,7 +19,7 @@
   </View> 
   <View style="overflow: auto; width: 50%;">
 <TextArea name="transcribed_json" transcription="false" toName="audio_url" 
-            rows="2" editable="true" 
+            rows="2" editable="true" maxSubmissions="1"
             perRegion="true" required="false" /> 
   </View>
               
