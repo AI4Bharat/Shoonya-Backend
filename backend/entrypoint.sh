ebda592f67a016bec552b2342468adace7037ec9
Abhigyan Raman <ramanabhigyan@gmail.com>
2023-01-21 13:37:16 +0000

    changes for migrating

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python manage.py makemigrations organizations
-python manage.py makemigrations workspaces
-python manage.py migrate
-python manage.py collectstatic --no-input -v 2 --clear
+# python manage.py makemigrations organizations
+# python manage.py makemigrations workspaces
+# python manage.py migrate
+# python manage.py collectstatic --no-input -v 2 --clear
 
 exec "$@"
