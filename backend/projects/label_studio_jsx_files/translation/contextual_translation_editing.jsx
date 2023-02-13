c14259eae4ef697b8caafddaf86743074cbcde9f
Ishvinder Sethi <ishvindersethi22@gmail.com>
2023-02-13 00:16:15 -0800

    Update contextual_translation_editing.jsx

--- backend/projects/label_studio_jsx_files/translation/contextual_translation_editing.jsx
+++ backend/projects/label_studio_jsx_files/translation/contextual_translation_editing.jsx
@@ -5,7 +5,7 @@
     <Header size="3" value="$output_language translation"/>
     <Header size="3" value="Machine translation"/>
     <Text name="input_text" value="$input_text"/>
-    <TextArea name="output_text" toName="input_text" value="$machine_translation" rows="5" transcription="false" showSubmitButton="true" maxSubmissions="1" editable="false" required="true"/>
+    <TextArea name="output_text" toName="input_text" value="$machine_translation" rows="5" transcription="true" showSubmitButton="true" maxSubmissions="1" editable="false" required="true"/>
     <Text name="machine_translation" value="$machine_translation"/>
   </View>
   <View style="font-size: large; display: grid; grid-template: auto; column-gap: 1em">
