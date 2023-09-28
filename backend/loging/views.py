ee98d0362d77974e3a8dbf364acd31ffd62a2aa6
suyogkokaje <suyogkokaje6@gmail.com>
2023-09-28 21:53:06 +0530

    feat: added transliteration logs

---
+++ backend/loging/views.py
@@ -0,0 +1,139 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from loging.serializers import TransliterationSerializer
+from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import generate_blob_sas, BlobSasPermissions
+from .tasks import retrieve_logs, send_email_with_url
+from users.models import User
+from rest_framework.permissions import IsAuthenticated
+import os   
+import json
+import datetime
+import re
+
+AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_CONNECTION_STRING")
+CONTAINER_NAME = os.getenv("CONTAINER_NAME")
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        return super().default(obj)
+
+def extract_account_key(connection_string):
+    pattern = r'AccountKey=([^;]+);'
+    match = re.search(pattern, connection_string)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+def create_empty_log_for_next_day(container_client):
+    current_date = datetime.date.today()
+    next_day = current_date + datetime.timedelta(days=1)
+    next_day_log_file = f"{next_day.isoformat()}.log"
+    
+    blob_client = container_client.get_blob_client(next_day_log_file)
+
+    if not blob_client.exists():
+        blob_client.upload_blob("[]", overwrite=True)
+
+class TransliterationSelectionViewSet(APIView):
+    # permission_classes = (IsAuthenticated,)
+    def post(self, request):
+        serializer = TransliterationSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            try:
+                current_date = datetime.date.today()
+                log_file_name = f"{current_date.isoformat()}.log"
+
+                blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
+                container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+                blob_client = container_client.get_blob_client(log_file_name)
+
+                if not blob_client.exists():
+                    blob_client.upload_blob("[]", overwrite=True) 
+
+                existing_data = blob_client.download_blob()
+                existing_content = existing_data.readall().decode('utf-8')
+                existing_json_data = json.loads(existing_content)
+                existing_json_data.append(data)
+
+                updated_content = json.dumps(existing_json_data, cls=CustomJSONEncoder)
+                blob_client.upload_blob(updated_content, overwrite=True)
+
+                create_empty_log_for_next_day(container_client)
+
+                central_blob_client = container_client.get_blob_client("central.log")
+
+                if not central_blob_client.exists():
+                    central_blob_client.upload_blob("[]", overwrite=True)
+
+                central_existing_data = central_blob_client.download_blob()
+                central_existing_content = central_existing_data.readall().decode('utf-8')
+                central_existing_json_data = json.loads(central_existing_content)
+                central_existing_json_data.append(data)
+
+                central_updated_content = json.dumps(central_existing_json_data, cls=CustomJSONEncoder)
+                central_blob_client.upload_blob(central_updated_content, overwrite=True)
+
+                return Response({"message": "Data stored in Azure Blob successfully"}, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({"message": "Failed to store data in Azure Blob", "error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        try:
+            user_id = request.query_params.get('user_id')
+            start_date_str = request.query_params.get('start_date')
+            end_date_str = request.query_params.get('end_date')
+
+            if user_id is None:
+                return Response({"message": "user_id is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+            try:
+                user = User.objects.get(id=user_id)
+            except User.DoesNotExist:
+                return Response({"message": "User with the provided user_id does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+            if start_date_str and end_date_str:
+                task = retrieve_logs.delay(start_date_str, end_date_str)
+            else:
+                task = retrieve_logs.delay(None, None)
+
+            result = task.get()
+            log_content = result.get("log_content", "")
+            file_name = result.get("file_name", "")
+            file_name_with_prefix = f"temp_{file_name}"
+
+            blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
+            container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+            blob_client = container_client.get_blob_client(file_name_with_prefix)
+            blob_client.upload_blob(log_content, overwrite=True)
+
+            expiry = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+            sas_token = generate_blob_sas(
+                container_name=CONTAINER_NAME,
+                blob_name=blob_client.blob_name,
+                account_name="shoonyastoragedevelop",
+                account_key=extract_account_key(AZURE_STORAGE_CONNECTION_STRING),
+                permission=BlobSasPermissions(read=True),
+                expiry=expiry,
+            )
+    
+            blob_url = f"https://shoonyastoragedevelop.blob.core.windows.net/{CONTAINER_NAME}/{blob_client.blob_name}?{sas_token}"
+            result = send_email_with_url.delay(user.email, blob_url)
+            task_status = result.get()
+
+            if task_status['status'] == 'success':
+                return Response({"message": task_status['message']}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": task_status['message']}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        except Exception as e:
+            return Response({"message": "Failed to store log content in blob", "error": str(e)},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
--- backend/shoonya_backend/settings.py
+++ backend/shoonya_backend/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     "projects",
     "tasks",
     "functions",
+    "loging",
     "corsheaders",
     "import_export",
     "django_celery_results",
