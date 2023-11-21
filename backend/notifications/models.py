ddbc9b6c8c2b05e0df6219301362764255d9d81a
Mustafa Asad <87380398+MustafaAsad198@users.noreply.github.com>
2023-11-21 11:16:25 +0530

    Update models.py

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -3,7 +3,11 @@ from users.models import User
 
 # Create your models here.
 PUBLISH_PROJECT = "publish_project"
-NOTIF_TYPES = ((PUBLISH_PROJECT, "Publish Project"),)
+TASK_UPADTE='task_update'
+NOTIF_TYPES = (
+                (PUBLISH_PROJECT, "Publish Project"),
+                (TASK_UPADTE, "Task Update"),
+            )
 
 
 class Notification(models.Model):
@@ -13,8 +17,8 @@ class Notification(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     priority = models.IntegerField(default=1)
     title = models.CharField(max_length=200)
-    on_click = models.URLField(blank=True, null=True)
-    metadata_json = models.JSONField(blank=True, null=True)
+    on_click = models.URLField(blank=True,null=True)
+    metadata_json = models.JSONField(blank=True,null=True)
 
     def __str__(self) -> str:
         return f"{self.title} notification"
