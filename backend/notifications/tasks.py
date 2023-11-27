5f8034c9859b8549760ecca9da9413f7721517c1
Mustafa Asad <87380398+MustafaAsad198@users.noreply.github.com>
2023-11-28 00:30:17 +0530

    Update tasks.py

--- backend/notifications/tasks.py
+++ backend/notifications/tasks.py
@@ -8,15 +8,15 @@ from django.db import models
 from datetime import datetime
 
 @shared_task
-def createNotificationHandler(user_id,title,project_id,notification_type, annotators_ids,reviewers_ids,super_checkers_ids,project_workspace_managers_ids,dataset_members_ids):
+def createNotificationHandler(title,project_id,notification_type,users_ids):
     """this is called after project has published"""
     if notification_type == "publish_project":
-        createNotificationPublishProject(title, notification_type, annotators_ids,reviewers_ids,super_checkers_ids,project_workspace_managers_ids)
+        createNotificationPublishProject(title, notification_type, users_ids)
     elif notification_type == "task_rejection":
-        createNotificationTaskRejection(user_id,title,project_id, notification_type, annotators_ids,reviewers_ids,super_checkers_ids,project_workspace_managers_ids)
+        createNotificationTaskRejection(title,project_id, notification_type, users_ids)
         # this will be called for task one, yet to create it when aggregation part is done
     elif notification_type=='dataset_create':
-        createNotificationDataset(title, notification_type, dataset_members_ids)
+        createNotificationDataset(title, notification_type, users_ids)
     else:
         print("Cannot create notifications")
     
@@ -35,42 +35,24 @@ def deleteNotification(user):
                 excess_notification.delete()
 
 @shared_task
-def createNotificationPublishProject(title,notification_type,annotators_ids,reviewers_ids,super_checkers_ids,project_workspace_managers_ids):
+def createNotificationPublishProject(title,notification_type,users_ids):
     new_notif = Notification(
         notification_type=notification_type,
         title=title,
         metadata_json="null",
     )
     new_notif.save()
-    for a_id in annotators_ids:
+    for u_id in users_ids:
         try:
-            receiver_user = User.objects.get(id=a_id)
+            receiver_user = User.objects.get(id=u_id)
             new_notif.reciever_user_id.add(receiver_user)
         except Exception as e:
-            return HttpResponse(f"Bad Request. User with ID: {a_id} does not exist.")
-    for r_id in reviewers_ids:
-        try:
-            receiver_user = User.objects.get(id=r_id)
-            new_notif.reciever_user_id.add(receiver_user)
-        except Exception as e:
-            return HttpResponse(f"Bad Request. User with ID: {r_id} does not exist.")
-    for s_id in super_checkers_ids:
-        try:
-            receiver_user = User.objects.get(id=s_id)
-            new_notif.reciever_user_id.add(receiver_user)
-        except Exception as e:
-            return HttpResponse(f"Bad Request. User with ID: {s_id} does not exist.")
-    for m_id in project_workspace_managers_ids:
-        try:
-            receiver_user = User.objects.get(id=m_id)
-            new_notif.reciever_user_id.add(receiver_user)
-        except Exception as e:
-            return HttpResponse(f"Bad Request. User with ID: {m_id} does not exist.")
+            return HttpResponse(f"Bad Request. User with ID: {u_id} does not exist.")
     print(f"Notification successfully created- {title}")
 
 
 @shared_task
-def createNotificationDataset(title,project_type,members_ids):
+def createNotificationDataset(title,project_type,users_ids):
     '''this function is for creating notifications when dataset is created and members are users associated with it'''
     new_notif = Notification(
         notification_type=project_type,
@@ -78,7 +60,7 @@ def createNotificationDataset(title,project_type,members_ids):
         metadata_json="null",
     )
     new_notif.save()
-    for m_id in members_ids:
+    for m_id in users_ids:
         try:
             receiver_user = User.objects.get(id=m_id)
             new_notif.reciever_user_id.add(receiver_user)
@@ -87,35 +69,26 @@ def createNotificationDataset(title,project_type,members_ids):
     print(f"Notification successfully created- {title}")
 
 @shared_task
-def createNotificationTaskRejection(user_id,title,project_id, notification_type, annotators_ids,reviewers_ids,super_checkers_ids,project_workspace_managers_ids):
-    existing_notif=Notification.objects.filter(notification_type='task_reject',title__icontains=str(project_id)).first()
-    if existing_notif:
-        existing_notif.created_at=datetime.now()
-        existing_notif.save()
-    else:
-        new_notif=Notification(
-            notification_type=notification_type,
-            title=title,
-            metadata_json='null',
-        )
-        new_notif.save()
-        for a_id in annotators_ids:
-            try:
-                receiver_user = User.objects.get(id=a_id)
-                new_notif.reciever_user_id.add(receiver_user)
-            except Exception as e:
-                return HttpResponse(f"Bad Request. User with ID: {a_id} does not exist.")
-        for r_id in reviewers_ids:
-            try:
-                receiver_user = User.objects.get(id=r_id)
-                new_notif.reciever_user_id.add(receiver_user)
-            except Exception as e:
-                return HttpResponse(f"Bad Request. User with ID: {r_id} does not exist.")
-        for s_id in super_checkers_ids:
-            try:
-                receiver_user = User.objects.get(id=s_id)
-                new_notif.reciever_user_id.add(receiver_user)
-            except Exception as e:
-                return HttpResponse(f"Bad Request. User with ID: {s_id} does not exist.")
-    print(existing_notif)
+def createNotificationTaskRejection(title,project_id, notification_type, users_ids):
+    users_ids.sort()
+    existing_notif=Notification.objects.filter(notification_type='task_reject',title__icontains=str(project_id))
+    for en in existing_notif:
+        existing_notif_receivers=en.reciever_user_id.all()
+        existing_notif_receivers_ids=[e.id for e in existing_notif_receivers]
+        if users_ids==sorted(existing_notif_receivers_ids):
+            existing_notif.created_at=datetime.now()
+            existing_notif.save()
+        else:
+            new_notif=Notification(
+                notification_type=notification_type,
+                title=title,
+                metadata_json='null',
+            )
+            new_notif.save()
+            for u_id in users_ids:
+                try:
+                    receiver_user = User.objects.get(id=u_id)
+                    new_notif.reciever_user_id.add(receiver_user)
+                except Exception as e:
+                    return HttpResponse(f"Bad Request. User with ID: {u_id} does not exist.")
     return HttpResponse(f"Notification aggregated successfully")
