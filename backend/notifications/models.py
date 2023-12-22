e514b8a03715d0bc38f49801077e1c6d2aedd3eb
Kunal Tiwary <kunaltiwary7@gmail.com>
2023-12-22 12:47:44 +0530

    added help text

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -11,15 +11,35 @@ NOTIF_TYPES = (
 
 
 class Notification(models.Model):
-    id = models.AutoField(primary_key=True)
-    reciever_user_id = models.ManyToManyField(User, blank=True)
-    notification_type = models.CharField(choices=NOTIF_TYPES, max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-    priority = models.IntegerField(default=1)
-    title = models.CharField(max_length=200)
-    on_click = models.URLField(blank=True, null=True)
-    metadata_json = models.JSONField(blank=True, null=True)
-    seen_json = models.JSONField(blank=True, null=True)
+    id = models.AutoField(
+        primary_key=True, help_text="Auto-incremented unique identifier."
+    )
+    reciever_user_id = models.ManyToManyField(
+        User, blank=True, help_text="Users who will receive the notification."
+    )
+    notification_type = models.CharField(
+        choices=NOTIF_TYPES, max_length=200, help_text="Type of notification."
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, help_text="Date and time when the notification was created."
+    )
+    priority = models.IntegerField(
+        default=1, help_text="Priority level of the notification."
+    )
+    title = models.CharField(max_length=200, help_text="Title of the notification.")
+    on_click = models.URLField(
+        blank=True,
+        null=True,
+        help_text="URL to be opened when the notification is clicked.",
+    )
+    metadata_json = models.JSONField(
+        blank=True, null=True, help_text="Additional metadata in JSON format."
+    )
+    seen_json = models.JSONField(
+        blank=True,
+        null=True,
+        help_text="JSON field to store information about whether the notification has been seen.",
+    )
 
     class Meta:
         ordering = ("-created_at",)
