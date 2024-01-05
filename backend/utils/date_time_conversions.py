c51cedc92388c73516e871d8f9f0143caa2f258a
Ishvinder Sethi <ishvindersethi22@gmail.com>
2024-01-05 16:45:17 +0530

    Update date_time_conversions.py

--- backend/utils/date_time_conversions.py
+++ backend/utils/date_time_conversions.py
@@ -11,3 +11,13 @@ def utc_to_ist(utc_time):
     central = utc.astimezone(to_zone)
     s = central.strftime("%d-%m-%Y %H:%M:%S")
     return s
+
+def ist_to_utc(ist_time, date_time_format):
+    to_zone = tz.gettz("UTC")
+    from_zone = tz.gettz("Asia/Kolkata")
+
+    ist = ist_time.replace(tzinfo=from_zone)
+
+    central = ist.astimezone(to_zone)
+    s = central.strftime(date_time_format)
+    return s
