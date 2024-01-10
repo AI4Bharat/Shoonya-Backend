aa398f4945084e37133a45918e79b7886485bdce
Kartik Virendra Rajput <88619994+kartikvirendrar@users.noreply.github.com>
2024-01-10 10:21:56 +0530

    added changes for new OCRSegmentCategorization project type

--- backend/projects/annotation_registry.py
+++ backend/projects/annotation_registry.py
@@ -130,6 +130,28 @@ ANNOTATION_REGISTRY_DICT = {
             "type": ["textarea", "labels", "textarea"],
         },
     },
+    "OCRSegmentCategorization": {
+        "ocr_seg_cat_json": {
+            "to_name": "image_url",
+            "from_name": [
+                "annotation_bboxes",
+                "annotation_labels",
+                "annotation_transcripts",
+            ],
+            "type": ["textarea", "labels", "textarea"],
+        },
+    },
+    "OCRSegmentCategorizationEditing": {
+        "ocr_seg_cat_json": {
+            "to_name": "image_url",
+            "from_name": [
+                "annotation_bboxes",
+                "annotation_labels",
+                "annotation_transcripts",
+            ],
+            "type": ["textarea", "labels", "textarea"],
+        },
+    },
     "AcousticNormalisedTranscriptionEditing": {
         "transcribed_json": {
             "to_name": "audio_url",
