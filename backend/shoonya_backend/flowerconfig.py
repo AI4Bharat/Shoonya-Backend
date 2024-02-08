cf88c3d682889b8a9bd40cb8e0b07e73a58571c4
aditya-gitte <adityagitte.dev@gmail.com>
2024-02-09 03:00:43 +0530

    added broker to config and removed broker from compose

--- backend/shoonya_backend/flowerconfig.py
+++ backend/shoonya_backend/flowerconfig.py
@@ -6,7 +6,7 @@ load_dotenv()
 address = os.getenv("FLOWER_ADDRESS")
 port = int(os.getenv("FLOWER_PORT"))
 broker_url = os.getenv("CELERY_BROKER_URL")
-
+broker = os.getenv("CELERY_BROKER_URL")
 # Enable basic authentication
 flower_username = os.getenv("FLOWER_USERNAME")
 flower_password = os.getenv("FLOWER_PASSWORD")
