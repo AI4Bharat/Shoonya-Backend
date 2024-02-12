3ede5dbc2809199c00bd70c1e9975ef5c83e0d24
aditya-gitte <adityagitte.dev@gmail.com>
2024-02-12 16:46:37 +0530

    added username and password through env in compose

--- backend/shoonya_backend/flowerconfig.py
+++ backend/shoonya_backend/flowerconfig.py
@@ -7,7 +7,8 @@ address = os.getenv("FLOWER_ADDRESS")
 port = int(os.getenv("FLOWER_PORT"))
 broker_url = os.getenv("CELERY_BROKER_URL")
 broker = os.getenv("CELERY_BROKER_URL")
+
 # Enable basic authentication
 flower_username = os.getenv("FLOWER_USERNAME")
 flower_password = os.getenv("FLOWER_PASSWORD")
-basic_auth = [f"{flower_username}:{flower_password}"]
+flower_auth = f"{flower_username}:{flower_password}"
