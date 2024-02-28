3527daa63731bfea58833591c3d8ad349856696b
Kunal Tiwary <kunaltiwary7@gmail.com>
2024-02-28 12:47:07 +0530

    resolving conflicts

--- backend/projects/utils.py
+++ backend/projects/utils.py
@@ -365,6 +365,12 @@ def process_ocr_results(
         is_OCRSegmentCategorization or is_OCRSegmentCategorizationEditing,
         False,
     )
+    if is_OCRSegmentCategorization or is_OCRSegmentCategorizationEditing:
+        bboxes_relation_json = []
+        for ann in annotation_result:
+            if "type" in ann and ann["type"] == "relation":
+                bboxes_relation_json.append(ann)
+        task["data"]["bboxes_relation_json"] = bboxes_relation_json
 
 
 def process_task(
