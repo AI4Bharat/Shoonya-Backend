a081a109acb4e3d75c516c69c632dff78a2ac0ea
suyogkokaje <suyogkokaje6@gmail.com>
2024-03-11 15:17:47 +0530

    fix:minor changes

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -2913,7 +2913,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
         Unassigns all labeled tasks from a superchecker.
         """
         user_type = "superchecker"
-        user, response = get_status_from_query_params(request, user_type)
+        user, response = get_user_from_query_params(request, user_type,pk)
         if response != None:
             return response
 
