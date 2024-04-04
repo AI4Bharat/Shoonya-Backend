a0c89fba86d515c5c12c582f0aa479975bdb8148
Kunal Tiwary <kunaltiwary7@gmail.com>
2024-04-04 06:25:31 +0000

    converting meta_info_model to model in get_llm_output()

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -2326,7 +2326,7 @@ def get_llm_output(prompt, task, annotation, project_metadata_json):
 
     # GET MODEL OUTPUT
     history = ann_result
-    model = task.data["meta_info_model"]
+    model = task.data["model"]
     return get_model_output(
         "We will be rendering your response on a frontend. so please add spaces or indentation or nextline chars or "
         "bullet or numberings etc. suitably for code or the text. wherever required.",
