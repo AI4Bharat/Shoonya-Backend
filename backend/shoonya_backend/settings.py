56a250d43bbd5bc006bacb4655fa69df43ada68a
Ishvinder Sethi <ishvindersethi22@gmail.com>
2024-04-16 15:02:22 +0530

    Update settings.py

--- backend/shoonya_backend/settings.py
+++ backend/shoonya_backend/settings.py
@@ -189,7 +189,7 @@ REST_FRAMEWORK = {
 # Email Settings
 EMAIL_BACKEND = "django_smtp_ssl.SSLEmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = 465
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("SMTP_USERNAME")
 EMAIL_HOST_PASSWORD = os.getenv("SMTP_PASSWORD")
 EMAIL_USE_TLS = True
