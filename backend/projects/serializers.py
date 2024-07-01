74a5c659c2b13ec115d72601882cb5a32c4e4d7e
Kunal Tiwary <kunaltiwary7@gmail.com>
2024-07-01 13:36:24 +0530

    added max_pull_count_per_user in serialiser

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -46,6 +46,7 @@ class ProjectSerializer(serializers.ModelSerializer):
             "revision_loop_count",
             "k_value",
             "metadata_json",
+            "max_pull_count_per_user",
         ]
 
 
