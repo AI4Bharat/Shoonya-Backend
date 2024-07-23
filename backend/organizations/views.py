9f380cf2951de986ef1b1b8dfce4b1b9d385783e
Rahul-538 <krrahul803213@gmail.com>
2024-07-23 08:52:11 +0530

    Resolved Error-500 in meta analytics

--- backend/organizations/views.py
+++ backend/organizations/views.py
@@ -2457,8 +2457,10 @@ class OrganizationPublicViewSet(viewsets.ModelViewSet):
                             "ann_cumulative_word_count": ann_word_count,
                             "rew_cumulative_word_count": rew_word_count,
                         }
-
-                general_lang.append(other_language)
+                    else:
+                        other_language= None
+                if other_language:
+                     general_lang.append(other_language)
             try:
                 final_result = sorted(
                     general_lang, key=lambda x: x["language"], reverse=False
