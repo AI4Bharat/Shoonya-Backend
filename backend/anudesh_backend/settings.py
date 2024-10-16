2b8885f7be72c0f0b67b48d9606ba604a15e7df2
Kunal Tiwary <kunaltiwary7@gmail.com>
2024-10-16 11:01:41 +0530

    added MultipleInteractionEvaluation

--- backend/anudesh_backend/settings.py
+++ backend/anudesh_backend/settings.py
@@ -295,7 +295,7 @@ CELERY_IMPORTS = ("users.tasks",)
 
 # Celery settings
 CELERY_TIMEZONE = "Asia/Kolkata"
-CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 
