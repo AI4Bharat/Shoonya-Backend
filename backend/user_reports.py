5d9e142e4c0dd902e7c833db8cf2394ffc6c3dbb
Gree606 <sleya7110@gmail.com>
2025-04-03 19:08:08 +0530

    to remove bbox from non ocr projects

--- backend/user_reports.py
+++ backend/user_reports.py
@@ -99,7 +99,7 @@ def calculate_reports():
 
         if user in final_annot_unique_list:
             data = {
-                "user_id": userId,
+                "user_id": 1,
                 "project_type": "all",
                 "reports_type": "annotation",
                 "start_date": yest_date,
@@ -353,9 +353,10 @@ def calculate_reports():
             "Daily Annotation and Review Reports",
             message,
             settings.DEFAULT_FROM_EMAIL,
-            [user.email],
+            ['sleya7110@gmail.com'],
             html_message=email_to_send,
         )
+        break
         
         
 
