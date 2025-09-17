0fd93647cdd24d9fec5c1abd8d1170a101fa2ffb
munishmangla98 <munishmangla98@gmail.com>
2025-09-17 15:10:30 +0530

    Notification API cal optimization done

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -1,9 +1,16 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from .views import *
 
+router = DefaultRouter()
+router.register(r'notifications', NotificationViewSet, basename='notifications')
 
 urlpatterns = [
     path("", viewNotifications, name="view_notification"),
     path("create", createNotification, name="create_notification"),
     path("changeState", mark_seen, name="mark_seen"),
+     path("unread/", allunreadNotifications, name="unread-notifications"),
+
+    # include the viewset routes
+    path("", include(router.urls)),
 ]
