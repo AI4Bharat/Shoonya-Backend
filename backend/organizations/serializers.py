42e445a2c2f70a831a71b5746e24a789208b34ca
munishmangla98 <munishmangla98@gmail.com>
2025-12-06 20:29:03 +0530

    admin fix

--- backend/organizations/serializers.py
+++ backend/organizations/serializers.py
@@ -23,5 +23,5 @@ class OrganizationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Organization
-        fields = ["id", "title", "email_domain_name", "created_by", "created_at"]
-        read_only_fields = ["id", "created_by", "created_at"]
+        fields = ["id"]
+        # read_only_fields = ["id"]
